4400a0e2af2563f46e8cc0d131eaf1e5402e62f5
Rishi N Desai <rishindesai@yahoo.com>
2021-05-14 00:13:35 +0530

    mysql connection and initial schema execution

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 require("dotenv").config({ path: __dirname + "/.env" });
+const { executeQuery } = require("./db");
+const fs = require("fs").promises;
 
 const translationRoutes = require("./routes/translation");
 
@@ -8,7 +10,25 @@ app.use(express.json());
 
 app.use("/api/translate", translationRoutes);
 
+const init = async () => {
+    try {
+        let queries = await fs.readFile("./db/schema.sql", "utf-8");
+        queries = queries.replace(/\r|\n/g, "").split(";");
+        queries.pop();
+
+        await Promise.all(
+            queries.map(async (query) => {
+                await executeQuery(query);
+            })
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 const port = 3001;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+init().then(() => {
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
 });
