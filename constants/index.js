6f40cd262bcc5c6cd8dc3d3ac085ac6be0c70cc3
Rishi N Desai <rishindesai@yahoo.com>
2021-05-15 22:53:08 +0530

    added related languages by region for use in smart pre-caching

--- constants/index.js
+++ constants/index.js
@@ -1,2 +1,8 @@
 exports.translationBaseURL = "https://api.cognitive.microsofttranslator.com";
 exports.translationServiceLocation = "centralindia";
+
+// related languages by region to use during smart pre-caching
+exports.regions = {
+    "south-asian": new Set(["hi", "gu", "bn", "kn", "ta"]),
+    "european": new Set(["en", "fr", "es", "nl", "de"]),
+};
